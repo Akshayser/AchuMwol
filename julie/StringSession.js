c0d716d2204e71f76b858153b468c19f700f6a2f
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-11-02 18:46:54 +0530

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'JulieMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'AchuMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
