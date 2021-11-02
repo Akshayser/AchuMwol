619a4cd8c536d6660f1742910baf02f7429a693d
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-11-02 17:23:59 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,7 +137,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Julie Mwol working!')
+            chalk.green.bold('✅ Achu Mwol is working!')
         );
         await new Promise(r => setTimeout(r, 1100));
 
@@ -399,8 +399,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ 𝕁𝕦𝕝𝕚𝕖𝕄𝕨𝕠𝕝 ~______~*' +
-                                    '\n\n*🧞‍♂️ ' + error + '*\n\n```Report errors\njoin with +91 \nchat.whatsapp.com/Gthpm3ob1y16Oh5qv0xSSr``` ' 
+                                await conn.sendMessage(conn.user.jid, '*~_________~ AchuMwol ~______~*' +
+                                    '\n\n*🧞‍♂️ ' + error + '*\n\n```Report errors\njoin with +91 \nhttps://chat.whatsapp.com/EtCx3zSWEnkCxdEIsLWnKD``` ' 
                                     , MessageType.text);
                             }
                         }
