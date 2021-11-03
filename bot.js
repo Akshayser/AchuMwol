0096c6e203f3cb7fc0bfb3f2f94a42c89031a4a4
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-11-03 15:36:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,9 +70,8 @@ async function whatsAsena () {
     
     
     const conn = new WAConnection();
-    const res = await axios('https://untitled-1ouyr1rszhaw.runkit.sh/web')
-    conn.version = res.data.version
-    const Session = new StringSession();
+    conn.version = [2,2142,12];
+const Session = new StringSession();
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
