ef34c9c0097fe9e62395ecefc9f047a9e5972dc3
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-11-03 20:25:17 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -893,7 +893,7 @@ else if (config.WORKTYPE == 'public') {
             return;
     
         let 
-            LANG = 'ml',
+            LANG = 'az',
             ttsMessage = match[1],
             SPEED = 1.0
 
