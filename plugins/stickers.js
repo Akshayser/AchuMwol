82edeccba9285038b629fc9da7ad44c3b98ef61e
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-11-03 18:43:06 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -1,81 +1,82 @@
-/* Copyright (C) 2020 Yusuf Usta.
+const Julie = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
+const Config = require('../config');
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
+const Language = require('../language');
+const Lang = Language.getString('sticker');
 
-WhatsAsena - Yusuf Usta
-*/
-const Asena = require("../Utilis/events");
-const { MessageType, Mimetype } = require("@adiwajshing/baileys");
-const Language = require("../language");
-const { webpToMp4 } = require("../Utilis/download");
-const { sticker, addExif } = require("../Utilis/fFmpeg");
-const Lang = Language.getString("sticker");
+if (Config.WORKTYPE == 'private') {
+    Julie.addCommand({pattern: 'sticker$', fromMe: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
 
-Asena.addCommand(
-  { pattern: "sticker ?(.*)", fromMe: true, desc: Lang.STICKER_DESC },
-  async (message, match) => {
-    if (
-      !message.reply_message ||
-      (!message.reply_message.video && !message.reply_message.image)
-    )
-      return await message.sendMessage(Lang.NEED_REPLY);
-    return await message.sendMessage(
-      await sticker(
-        "imagesticker",
-        await message.reply_message.downloadAndSaveMediaMessage("sticker"),
-        message.reply_message.image
-          ? 1
-          : message.reply_message.seconds < 10
-            ? 2
-            : 3,
-        match
-      ),
-      {
-        mimetype: Mimetype.webp,
-        quoted: message.quoted,
-        isAnimated: message.reply_message.video,
-      },
-      MessageType.sticker
-    );
-  }
-);
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
 
-Asena.addCommand(
-  { pattern: "mp4", fromMe: true, desc: Lang.MP4_DESC },
-  async (message, match) => {
-    if (
-      !message.reply_message.sticker ||
-      !message.reply_message ||
-      !message.reply_message.animated
-    )
-      return await message.sendMessage(Lang.MP4_NEED_REPLY);
-    return await message.sendMessage(
-      await webpToMp4(
-        await message.reply_message.downloadAndSaveMediaMessage("mp4")
-      ),
-      { quoted: message.quoted },
-      MessageType.video
-    );
-  }
-);
+        if (message.reply_message.video === false && message.reply_message.image) {
+            ffmpeg(location)
+                .outputOptions(["-y", "-vcodec libwebp"])
+                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .save('st.webp')
+                .on('end', async () => {
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
-Asena.addCommand(
-  { pattern: "take ?(.*)", fromMe: true, desc: Lang.TAKE_DESC },
-  async (message, match) => {
-    if (!message.reply_message.sticker || !message.reply_message)
-      return await message.sendMessage(Lang.TAKE_NEED_REPLY);
-    return await message.sendMessage(
-      await addExif(
-        await message.reply_message.downloadAndSaveMediaMessage("take"),
-        match
-      ),
-      {
-        mimetype: Mimetype.webp,
-        isAnimated: message.reply_message.animated,
-        quoted: message.quoted,
-      },
-      MessageType.sticker
-    );
-  }
-);
+        }
+
+        ffmpeg(location)
+            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
+            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+            .save('sticker.webp')
+            .on('end', async () => {
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Julie.addCommand({pattern: 'sticker$', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        if (message.reply_message.video === false && message.reply_message.image) {
+            ffmpeg(location)
+                .outputOptions(["-y", "-vcodec libwebp"])
+                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .save('st.webp')
+                .on('end', async () => {
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+
+        }
+
+        ffmpeg(location)
+            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
+            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+            .save('sticker.webp')
+            .on('end', async () => {
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+}
