5efe5bf450b816c69ed46606804bdf3bcb3565b5
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-11-09 22:36:16 +0530

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -89,7 +89,7 @@ Julie.addCommand({pattern: 'add(?: |$)(.*)', fromMe: true, dontAddCommandList: t
     }
 }));
 
-Julie.addCommand({pattern: 'promote ?(.*)', fromMe: true, dontAddCommandList: true, onlyGroup: true, desc: Lang.PROMOTE_DESC}, (async (message, match) => {    
+Julie.addCommand({pattern: 'admin ?(.*)', fromMe: true, dontAddCommandList: true, onlyGroup: true, desc: Lang.PROMOTE_DESC}, (async (message, match) => {    
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
 
