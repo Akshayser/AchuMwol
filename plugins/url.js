05d65696830f5034f267cc97703d9e30cd0167cd
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-11-22 18:30:34 +0530

    ```THANKS TO SOURAV```

--- plugins/url.js
+++ plugins/url.js
@@ -5,6 +5,6 @@ const Config = require('../config');
 let sk = Config.WORKTYPE == 'public' ? false : true
 skl.addCommand({pattern: 'url ?(.*)', fromMe: sk}, async (message, match) => {
 
-    await message.sendMessage('Use this site to upload images and get URL 👇:\n\n https://souravkl11.github.io/image-uploader/')
+    await message.sendMessage('Use this site to upload images and get URL 👇:\n\n https://github.com/Akshayser/image-uploader/')
 
         });
